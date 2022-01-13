ab1b41e24d8c5019470b41c539a72d8e98467459
Wolke Letzet <wolke.letzet@gmail.com>
2022-01-13 17:53:41 -0300

    2

--- resources/views/comp/create.blade.php
+++ resources/views/comp/create.blade.php
@@ -1,12 +1,21 @@
 <form action="">
 
+
+    <div>
+        <label for="oficina"></label>}
+        <select name="oficina" id="">
+
+            @for ($i = 0; $i < 5; $i++)
+                <option value=""{{$i}}></option>
+            @endfor
+            
+
+        </select>
+    </div>
     <div>
         <label for="encargado">Encargado</label>
         <input name="encargado" type="text">
     </div> 
-    <div>
-        <label for=""></label>}
-        {!! Form::select($name, $list, $selected, [$options]) !!}
-    </div>
+    
 
 </form>
