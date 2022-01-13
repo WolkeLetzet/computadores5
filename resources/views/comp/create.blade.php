e8cd3f02fe6b14b180804b8b46a2828dfa71b866
Wolke Letzet <wolke.letzet@gmail.com>
2022-01-13 17:51:23 -0300

    2

---
+++ resources/views/comp/create.blade.php
@@ -0,0 +1,12 @@
+<form action="">
+
+    <div>
+        <label for="encargado">Encargado</label>
+        <input name="encargado" type="text">
+    </div> 
+    <div>
+        <label for=""></label>}
+        {!! Form::select($name, $list, $selected, [$options]) !!}
+    </div>
+
+</form>
