f98ac4b8470f67e4714936b86f6d7d68a25ccd76
Wolke Letzet <wolke.letzet@gmail.com>
2022-01-13 17:55:45 -0300

    2

--- resources/views/comp/create.blade.php
+++ resources/views/comp/create.blade.php
@@ -3,7 +3,7 @@
 
     <div>
         <label for="oficina"></label>}
-        <select name="oficina" id="">
+        <select name="oficina" id="oficina">
 
             @for ($i = 0; $i < 5; $i++)
                 <option value=""{{$i}}></option>
@@ -16,6 +16,29 @@
         <label for="encargado">Encargado</label>
         <input name="encargado" type="text">
     </div> 
+
+    <div>
+        <label for="tipoUso"></label>}
+        <select name="tipoUso" id="marca">
+
+            @for ($i = 0; $i < 5; $i++)
+                <option value=""{{$i}}></option>
+            @endfor
+            
+
+        </select>
+    </div>
     
+    <div>
+        <label for="marca"></label>}
+        <select name="marca" id="marca">
+
+            @for ($i = 0; $i < 5; $i++)
+                <option value=""{{$i}}></option>
+            @endfor
+            
+
+        </select>
+    </div>
 
 </form>
